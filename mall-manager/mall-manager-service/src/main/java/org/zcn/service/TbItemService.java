42da693aa797df5001746b0f75b0b0a62090982d
l592 <925935169@qq.com>
2020-06-16 10:23:31 +0800

    林湘彬 添加mall-manager 20206/16 10.23

---
+++ mall-manager/mall-manager-service/src/main/java/org/zcn/service/TbItemService.java
@@ -0,0 +1,7 @@
+package org.zcn.service;
+
+import org.zcn.utils.EasyUIDataGridResult;
+
+public interface TbItemService {
+      public EasyUIDataGridResult getTbItemList(Integer page,Integer rows); 
+}
